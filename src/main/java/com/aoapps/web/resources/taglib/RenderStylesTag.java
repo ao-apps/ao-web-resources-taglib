d1ca5d7262c0a8d499972753f21afd390ee21418
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:48:23 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/com/aoapps/web/resources/taglib/RenderStylesTag.java
+++ src/main/java/com/aoapps/web/resources/taglib/RenderStylesTag.java
@@ -44,143 +44,147 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class RenderStylesTag extends SimpleTagSupport {
 
-	private boolean application = false;
-	public void setApplication(boolean application) {
-		this.application = application;
-	}
+  private boolean application = false;
+  public void setApplication(boolean application) {
+    this.application = application;
+  }
 
-	private boolean session = true;
-	public void setSession(boolean session) {
-		this.session = session;
-	}
+  private boolean session = true;
+  public void setSession(boolean session) {
+    this.session = session;
+  }
 
-	private boolean request = true;
-	public void setRequest(boolean request) {
-		this.request = request;
-	}
+  private boolean request = true;
+  public void setRequest(boolean request) {
+    this.request = request;
+  }
 
-	private boolean page = true;
-	public void setPage(boolean page) {
-		this.page = page;
-	}
+  private boolean page = true;
+  public void setPage(boolean page) {
+    this.page = page;
+  }
 
-	private boolean registered = true;
-	public void setRegistered(boolean registered) {
-		this.registered = registered;
-	}
+  private boolean registered = true;
+  public void setRegistered(boolean registered) {
+    this.registered = registered;
+  }
 
-	private Set<Group.Name> activate;
-	public void setActivate(String activate) {
-		if(activate == null) {
-			this.activate = null;
-		} else {
-			Set<Group.Name> newActivates = new HashSet<>();
-			for(String name : Strings.splitCommaSpace(activate)) {
-				Group.Name group = new Group.Name(name);
-				// Activations take priority over deactivate when set in both attributes
-				if(deactivate != null) {
-					if(
-						deactivate.remove(group)
-						&& deactivate.isEmpty()
-					) {
-						deactivate = null;
-					}
-				}
-				newActivates.add(group);
-			}
-			this.activate = newActivates.isEmpty() ? null : newActivates;
-		}
-	}
+  private Set<Group.Name> activate;
+  public void setActivate(String activate) {
+    if (activate == null) {
+      this.activate = null;
+    } else {
+      Set<Group.Name> newActivates = new HashSet<>();
+      for (String name : Strings.splitCommaSpace(activate)) {
+        Group.Name group = new Group.Name(name);
+        // Activations take priority over deactivate when set in both attributes
+        if (deactivate != null) {
+          if (
+            deactivate.remove(group)
+            && deactivate.isEmpty()
+          ) {
+            deactivate = null;
+          }
+        }
+        newActivates.add(group);
+      }
+      this.activate = newActivates.isEmpty() ? null : newActivates;
+    }
+  }
 
-	private Set<Group.Name> deactivate;
-	public void setDeactivate(String daeactivate) {
-		if(daeactivate == null) {
-			this.deactivate = null;
-		} else {
-			Set<Group.Name> newDeactivates = new HashSet<>();
-			for(String name : Strings.splitCommaSpace(daeactivate)) {
-				Group.Name group = new Group.Name(name);
-				// Activations take priority over deactivate when set in both attributes
-				if(activate == null || !activate.contains(group)) {
-					newDeactivates.add(group);
-				}
-			}
-			this.deactivate = newDeactivates.isEmpty() ? null : newDeactivates;
-		}
-	}
+  private Set<Group.Name> deactivate;
+  public void setDeactivate(String daeactivate) {
+    if (daeactivate == null) {
+      this.deactivate = null;
+    } else {
+      Set<Group.Name> newDeactivates = new HashSet<>();
+      for (String name : Strings.splitCommaSpace(daeactivate)) {
+        Group.Name group = new Group.Name(name);
+        // Activations take priority over deactivate when set in both attributes
+        if (activate == null || !activate.contains(group)) {
+          newDeactivates.add(group);
+        }
+      }
+      this.deactivate = newDeactivates.isEmpty() ? null : newDeactivates;
+    }
+  }
 
-	void activate(Group.Name group) {
-		if(activate == null) activate = new HashSet<>();
-		activate.add(group);
-		if(
-			deactivate != null
-			&& deactivate.remove(group)
-			&& deactivate.isEmpty()
-		) {
-			deactivate = null;
-		}
-	}
+  void activate(Group.Name group) {
+    if (activate == null) {
+      activate = new HashSet<>();
+    }
+    activate.add(group);
+    if (
+      deactivate != null
+      && deactivate.remove(group)
+      && deactivate.isEmpty()
+    ) {
+      deactivate = null;
+    }
+  }
 
-	void deactivate(Group.Name group) {
-		if(deactivate == null) deactivate = new HashSet<>();
-		deactivate.add(group);
-		if(
-			activate != null
-			&& activate.remove(group)
-			&& activate.isEmpty()
-		) {
-			activate = null;
-		}
-	}
+  void deactivate(Group.Name group) {
+    if (deactivate == null) {
+      deactivate = new HashSet<>();
+    }
+    deactivate.add(group);
+    if (
+      activate != null
+      && activate.remove(group)
+      && activate.isEmpty()
+    ) {
+      activate = null;
+    }
+  }
 
-	@Override
-	public void doTag() throws JspException, IOException {
-		JspFragment body = getJspBody();
-		if(body != null) {
-			// Invoke body for any meta data, but discard any output
-			body.invoke(NullWriter.getInstance());
-		}
+  @Override
+  public void doTag() throws JspException, IOException {
+    JspFragment body = getJspBody();
+    if (body != null) {
+      // Invoke body for any meta data, but discard any output
+      body.invoke(NullWriter.getInstance());
+    }
 
-		PageContext pageContext = (PageContext)getJspContext();
-		ServletContext servletContext = pageContext.getServletContext();
-		HttpServletRequest httpRequest = (HttpServletRequest)pageContext.getRequest();
-		HttpServletResponse httpResponse = (HttpServletResponse)pageContext.getResponse();
+    PageContext pageContext = (PageContext)getJspContext();
+    ServletContext servletContext = pageContext.getServletContext();
+    HttpServletRequest httpRequest = (HttpServletRequest)pageContext.getRequest();
+    HttpServletResponse httpResponse = (HttpServletResponse)pageContext.getResponse();
 
-		Map<Group.Name, Boolean> activates;
-		if(activate == null && deactivate == null) {
-			activates = null;
-		} else {
-			activates = new HashMap<>();
-			if(activate != null) {
-				for(Group.Name group : activate) {
-					assert !activates.containsKey(group);
-					activates.put(group, true);
-				}
-			}
-			if(deactivate != null) {
-				for(Group.Name group : deactivate) {
-					assert !activates.containsKey(group);
-					activates.put(group, false);
-				}
-			}
-		}
-		Renderer.get(servletContext).renderStyles(
-			httpRequest,
-			httpResponse,
-			new DocumentEE(
-				servletContext,
-				httpRequest,
-				httpResponse,
-				pageContext.getOut(),
-				false, // Do not add extra newlines to JSP
-				false  // Do not add extra indentation to JSP
-			),
-			registered,
-			activates,
-			application ? RegistryEE.Application.get(servletContext)                : null,
-			session     ? RegistryEE.Session    .get(httpRequest.getSession(false)) : null,
-			request     ? RegistryEE.Request    .get(servletContext, httpRequest)   : null,
-			page        ? RegistryEE.Page  .get(httpRequest)                   : null
-		);
-	}
+    Map<Group.Name, Boolean> activates;
+    if (activate == null && deactivate == null) {
+      activates = null;
+    } else {
+      activates = new HashMap<>();
+      if (activate != null) {
+        for (Group.Name group : activate) {
+          assert !activates.containsKey(group);
+          activates.put(group, true);
+        }
+      }
+      if (deactivate != null) {
+        for (Group.Name group : deactivate) {
+          assert !activates.containsKey(group);
+          activates.put(group, false);
+        }
+      }
+    }
+    Renderer.get(servletContext).renderStyles(
+      httpRequest,
+      httpResponse,
+      new DocumentEE(
+        servletContext,
+        httpRequest,
+        httpResponse,
+        pageContext.getOut(),
+        false, // Do not add extra newlines to JSP
+        false  // Do not add extra indentation to JSP
+      ),
+      registered,
+      activates,
+      application ? RegistryEE.Application.get(servletContext)                : null,
+      session     ? RegistryEE.Session    .get(httpRequest.getSession(false)) : null,
+      request     ? RegistryEE.Request    .get(servletContext, httpRequest)   : null,
+      page        ? RegistryEE.Page  .get(httpRequest)                   : null
+    );
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * ao-web-resources-taglib - Web resource management in a JSP environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,13 +21,13 @@
  * along with ao-web-resources-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.web.resources.taglib {
-	exports com.aoapps.web.resources.taglib;
-	// Direct
-	requires com.aoapps.html.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-servlet</artifactId>
-	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
-	requires com.aoapps.web.resources.registry; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-registry</artifactId>
-	requires com.aoapps.web.resources.renderer; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-renderer</artifactId>
-	requires com.aoapps.web.resources.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-servlet</artifactId>
-	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
-	requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
+  exports com.aoapps.web.resources.taglib;
+  // Direct
+  requires com.aoapps.html.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-servlet</artifactId>
+  requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+  requires com.aoapps.web.resources.registry; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-registry</artifactId>
+  requires com.aoapps.web.resources.renderer; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-renderer</artifactId>
+  requires com.aoapps.web.resources.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-servlet</artifactId>
+  requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+  requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
 }
